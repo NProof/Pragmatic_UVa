53f38595c513a2cfb1e0fd87e1a9179d2c4da952
NProof <endles00000@gmail.com>
2018-08-18 17:50:26 +0800

    INPUT SETP

--- UVa10319_Manhattan/UVa10319_Manhattan.cpp
+++ UVa10319_Manhattan/UVa10319_Manhattan.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 // data structure
 struct City
 {
@@ -8,10 +10,32 @@ struct City
 
 struct Router
 {
-	int s1:6, a1:6, s2:5, a2:5;
+	int s1, a1, s2, a2;
 };
 
 int main()
 {
+	int n;
+	std::cin >> n;
+	#ifdef DEBUG
+	std::cout << n << std::endl;
+	#endif
+	while(n--)
+	{
+		City city;
+		std::cin >> city.S >> city.A >> city.m;
+		#ifdef DEBUG
+		std::cout << city.S << city.A << city.m << std::endl;
+		#endif
+		int m = city.m;
+		Router routers[m];
+		while(m--)
+		{
+			std::cin >> routers[m].s1 >> routers[m].a1 >> routers[m].s2 >> routers[m].a2;
+			#ifdef DEBUG
+			std::cout << routers[m].s1 << routers[m].a1 << routers[m].s2 << routers[m].a2 << std::endl;
+			#endif
+		}
+	}
 	return 0;
 }
