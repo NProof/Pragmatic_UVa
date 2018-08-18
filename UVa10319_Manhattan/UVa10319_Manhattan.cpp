bd61e8e99d08ff4fa0007fa4fea4d19d08d56bb2
NProof <endles00000@gmail.com>
2018-08-18 16:59:55 +0800

    add UVa10319_Manhattan.cpp

---
+++ UVa10319_Manhattan/UVa10319_Manhattan.cpp
@@ -0,0 +1,17 @@
+// data structure
+struct City
+{
+	int S; // 0 < S <= 30
+	int A; // 0 < A <= 30
+	int m; // 0 < m <= 200
+};
+
+struct Router
+{
+	int s1:6, a1:6, s2:5, a2:5;
+};
+
+int main()
+{
+	return 0;
+}
